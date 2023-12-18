ad761b60431d8840ae26d8eca2dea7340bf9ccbc
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-18 00:17:24 -0500

    Simplify kinship function

--- src/kinship.jl
+++ src/kinship.jl
@@ -3,27 +3,28 @@ phi(individual₁::PointerIndividual, individual₂::PointerIndividual)::Float64
 
 Computes the kinship coefficient between two individuals using pointers.
 """
-function phi(individual₁::PointerIndividual, individual₂::PointerIndividual)
+function phi(individual₁::PointerIndividual, individual₂::PointerIndividual)::Float64
     # Ported from GENLIB's Kinship
+    value = 0.
     if individual₁ == individual₂
-        value = 0.
-        if !isnothing(individual₁.father) & !isnothing(individual₁.mother)
-            value = phi(individual₁.father, individual₁.mother)
+        value += 1/2
+        if !isnothing(individual₁.father)
+            if !isnothing(individual₁.mother)
+                value += phi(individual₁.father, individual₁.mother) / 2
+            end
         end
-        return (1. + value) / 2.
+        return value
     end
     if individual₂.index > individual₁.index
         individual₁, individual₂ = individual₂, individual₁
     end
-    father_value = 0.
     if !isnothing(individual₁.father)
-        father_value = phi(individual₁.father, individual₂)
+        value += phi(individual₁.father, individual₂) / 2
     end
-    mother_value = 0.
     if !isnothing(individual₁.mother)
-        mother_value = phi(individual₁.mother, individual₂)
+        value += phi(individual₁.mother, individual₂) / 2
     end
-    return (father_value + mother_value) / 2.
+    return value
 end
 
 """
