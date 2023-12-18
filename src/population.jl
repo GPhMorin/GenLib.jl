aef5fb2f7ef45d393eebc02f15c56610fda4e55b
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-18 01:54:05 -0500

    Fix population function

--- src/population.jl
+++ src/population.jl
@@ -4,7 +4,7 @@ population(filename::String)::Dict{Int64, String}
 Reads a file with `filename` and returns a dictionary of populations.
 """
 function population(filename::String)::Dict{Int64, String}
-    dataset = CSV.read(filename, DataFrame, delim='\t', types=Dict(:ind => Int64, :father => Int64, :mother => Int64, :sex => Int8))
+    dataset = CSV.read(filename, DataFrame, delim='\t', types=Dict(:ind => Int64, :pop => String))
     population::Dict{Int64, String} = Dict()
     for row in eachrow(dataset)
         population[row.ind] = row.pop
