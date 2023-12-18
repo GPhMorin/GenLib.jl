e45916f95bd5390db283df0dcb83bd37c383a127
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-17 23:42:57 -0500

    Remove superfluous "/" in paths

--- src/GenLib.jl
+++ src/GenLib.jl
@@ -26,9 +26,9 @@ export genealogy,
        rec
 
 # GENLIB datasets
-const genea140 = "$(chop(pathof(GenLib), tail=13))/data/genea140.asc"
-const geneaJi = "$(chop(pathof(GenLib), tail=13))/data/geneaJi.asc"
-const pop140 = "$(chop(pathof(GenLib), tail=13))/data/pop140.asc"
+const genea140 = "$(chop(pathof(GenLib), tail=13))data/genea140.asc"
+const geneaJi = "$(chop(pathof(GenLib), tail=13))data/geneaJi.asc"
+const pop140 = "$(chop(pathof(GenLib), tail=13))data/pop140.asc"
 
 export genea140,
        geneaJi,
