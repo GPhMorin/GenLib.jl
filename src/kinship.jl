1fdc8c93da75bdaaaa31896d854aa2770c978316
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-18 00:31:42 -0500

    Put some of phi in comments

--- src/kinship.jl
+++ src/kinship.jl
@@ -5,26 +5,27 @@ Computes the kinship coefficient between two individuals using pointers.
 """
 function phi(individual₁::PointerIndividual, individual₂::PointerIndividual)::Float64
     # Ported from GENLIB's Kinship
+    # Remove comments if a child can have an unknown father but a known mother
     value = 0.
     if individual₁ == individual₂
         value += 1/2
-        if !isnothing(individual₁.father) & !isnothing(individual₁.mother)
+        if !isnothing(individual₁.father) # & !isnothing(individual₁.mother)
             value += phi(individual₁.father, individual₁.mother) / 2
         end
         return value
     elseif individual₂.index > individual₁.index
         if !isnothing(individual₂.father)
             value += phi(individual₂.father, individual₁) / 2
-        end
-        if !isnothing(individual₂.mother)
+        # end
+        # if !isnothing(individual₂.mother)
             value += phi(individual₂.mother, individual₁) / 2
         end
         return value
     else
         if !isnothing(individual₁.father)
             value += phi(individual₁.father, individual₂) / 2
-        end
-        if !isnothing(individual₁.mother)
+        # end
+        # if !isnothing(individual₁.mother)
             value += phi(individual₁.mother, individual₂) / 2
         end
         return value
