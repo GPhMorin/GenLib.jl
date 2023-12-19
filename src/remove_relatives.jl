a3bc4c1c6ca94939de332908c81011d94d262355
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-19 17:50:01 -0500

    Fix pass by reference

--- src/remove_relatives.jl
+++ src/remove_relatives.jl
@@ -1,69 +1,70 @@
 """
-remove_relatives!(probandIDs::Vector{Int64}, genealogy::Dict{Int64, Individual})::Vector{Int64}
+remove_relativeIDs!(probandIDs::Vector{Int64}, genealogy::Dict{Int64, Individual})::Vector{Int64}
 
 Takes a list of `probandIDs` and, according to a given `genealogy`,
 removes IDs of individuals who are first cousins or closer in the genealogy.
 """
-function remove_relatives(genealogy::Dict{Int64, Individual}, probandIDs::Vector{Int64})::Vector{Int64}
+function remove_relativeIDs!(probandIDs::Vector{Int64}, genealogy::Dict{Int64, Individual})::Vector{Int64}
     pointer = point(genealogy)
-    preserved_probands = Vector{Int64}()
-    for probandID in probandIDs
+    candidateIDs = copy(probandIDs)
+    empty!(probandIDs)
+    for probandID in candidateIDs
         proband = pointer[probandID]
-        relatives = Vector{Int64}()
+        relativeIDs = Vector{Int64}()
         for child in proband.children
-            push!(relatives, child.ID)
+            push!(relativeIDs, child.ID)
         end
         if !isnothing(proband.father)
-            push!(relatives, proband.father.ID)
+            push!(relativeIDs, proband.father.ID)
             for sibling in proband.father.children
-                push!(relatives, sibling.ID)
+                push!(relativeIDs, sibling.ID)
             end
             if !isnothing(proband.father.father)
-                push!(relatives, proband.father.father.ID)
+                push!(relativeIDs, proband.father.father.ID)
                 for avuncular in proband.father.father.children
-                    push!(relatives, avuncular.ID)
+                    push!(relativeIDs, avuncular.ID)
                     for cousin in avuncular.children
-                        push!(relatives, cousin.ID)
+                        push!(relativeIDs, cousin.ID)
                     end
                 end
             end
             if !isnothing(proband.father.mother)
-                push!(relatives, proband.father.mother.ID)
+                push!(relativeIDs, proband.father.mother.ID)
                 for avuncular in proband.father.mother.children
-                    push!(relatives, avuncular.ID)
+                    push!(relativeIDs, avuncular.ID)
                     for cousin in avuncular.children
-                        push!(relatives, cousin.ID)
+                        push!(relativeIDs, cousin.ID)
                     end
                 end
             end
         end
         if !isnothing(proband.mother)
-            push!(relatives, proband.mother.ID)
+            push!(relativeIDs, proband.mother.ID)
             for sibling in proband.mother.children
-                push!(relatives, sibling.ID)
+                push!(relativeIDs, sibling.ID)
             end
             if !isnothing(proband.mother.father)
-                push!(relatives, proband.mother.father.ID)
+                push!(relativeIDs, proband.mother.father.ID)
                 for avuncular in proband.mother.father.children
-                    push!(relatives, avuncular.ID)
+                    push!(relativeIDs, avuncular.ID)
                     for cousin in avuncular.children
-                        push!(relatives, cousin.ID)
+                        push!(relativeIDs, cousin.ID)
                     end
                 end
             end
             if !isnothing(proband.mother.mother)
-                push!(relatives, proband.mother.mother.ID)
+                push!(relativeIDs, proband.mother.mother.ID)
                 for avuncular in proband.mother.mother.children
-                    push!(relatives, avuncular.ID)
+                    push!(relativeIDs, avuncular.ID)
                     for cousin in avuncular.children
-                        push!(relatives, cousin.ID)
+                        push!(relativeIDs, cousin.ID)
                     end
                 end
             end
         end
-        if isempty(relatives ∩ preserved_probands)
-            push!(preserved_probands, proband.ID)
+        if isempty(relativeIDs ∩ probandIDs)
+            push!(probandIDs, proband.ID)
         end
     end
-    preserved_probands
+    probandIDs
 end
