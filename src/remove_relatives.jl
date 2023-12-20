33098434db5ef29b5bf744494b4ef1802cbbea0d
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-20 01:40:55 -0500

    New structure

--- src/remove_relatives.jl
+++ src/remove_relatives.jl
@@ -11,62 +11,27 @@ function remove_relatives!(probandIDs::Vector{Int64}, genealogy::Dict{Int64, Ind
     for probandID in candidateIDs
         proband = pointer[probandID]
         relativeIDs = Vector{Int64}()
-        for child in proband.children
-            push!(relativeIDs, child.ID)
-        end
-        if !isnothing(proband.father)
-            push!(relativeIDs, proband.father.ID)
-            for sibling in proband.father.children
-                push!(relativeIDs, sibling.ID)
-            end
-            if !isnothing(proband.father.father)
-                push!(relativeIDs, proband.father.father.ID)
-                for avuncular in proband.father.father.children
-                    push!(relativeIDs, avuncular.ID)
-                    for cousin in avuncular.children
-                        push!(relativeIDs, cousin.ID)
-                    end
-                end
-            end
-            if !isnothing(proband.father.mother)
-                push!(relativeIDs, proband.father.mother.ID)
-                for avuncular in proband.father.mother.children
-                    push!(relativeIDs, avuncular.ID)
-                    for cousin in avuncular.children
-                        push!(relativeIDs, cousin.ID)
-                    end
-                end
-            end
-        end
-        if !isnothing(proband.mother)
-            push!(relativeIDs, proband.mother.ID)
-            for sibling in proband.mother.children
-                push!(relativeIDs, sibling.ID)
-            end
-            if !isnothing(proband.mother.father)
-                push!(relativeIDs, proband.mother.father.ID)
-                for avuncular in proband.mother.father.children
-                    push!(relativeIDs, avuncular.ID)
-                    for cousin in avuncular.children
-                        push!(relativeIDs, cousin.ID)
-                    end
-                end
-            end
-            if !isnothing(proband.mother.mother)
-                push!(relativeIDs, proband.mother.mother.ID)
-                for avuncular in proband.mother.mother.children
-                    push!(relativeIDs, avuncular.ID)
-                    for cousin in avuncular.children
-                        push!(relativeIDs, cousin.ID)
-                    end
-                end
-            end
-        end
+        add_relatives!(relativeIDs, proband, Int8(0))
         if isempty(relativeIDs ∩ probandIDs)
             push!(probandIDs, proband.ID)
         end
     end
     probandIDs
+end
+
+function add_relatives!(relativeIDs::Vector{Int64}, individual::PointerIndividual, depth::Int8)
+    push!(relativeIDs, individual.ID)
+    if depth < 4
+        if !isnothing(individual.father)
+            add_relatives!(relativeIDs, individual.father, depth += Int8(1))
+        end
+        if !isnothing(individual.mother)
+            add_relatives!(relativeIDs, individual.mother, depth += Int8(1))
+        end
+        for child in individual.children
+            add_relatives!(relativeIDs, child, depth += Int8(1))
+        end
+    end
 end
 
 function remove_relatives(genealogy::Dict{Int64, Individual}, probandIDs::Vector{Int64}, threshold::Float64 = 1/16)::Vector{Int64}
