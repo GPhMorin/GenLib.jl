09f8cb1d65008ad7b670239f19770800d51bfa63
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-19 21:58:25 -0500

    Add comments

--- src/occurrence.jl
+++ src/occurrence.jl
@@ -1,3 +1,11 @@
+"""
+occ(genealogy::Dict{Int64, Individual}, probandIDs::Vector{Int64}, ancestorIDs::Vector{Int64}; type::Symbol = :ind)::Matrix{Int64}
+
+Takes a `genealogy`, a vector of `probandIDs`, a vector of `ancestorIDs`
+and optionally a `type` (`:ind` or `:total`) and returns a matrix of occurrences.
+If the type is `:ind` (default), then the matrix corresponds to *probandIDs* × *ancestorIDs*.
+If the type is `:total`, then the matrix corresponds to *ancestorIDs* × 1.
+"""
 function occ(genealogy::Dict{Int64, Individual}, probandIDs::Vector{Int64}, ancestorIDs::Vector{Int64}; type::Symbol = :ind)::Matrix{Int64}
     occurrence_matrix = Matrix{Int64}(undef, length(probandIDs), length(ancestorIDs))
     pointer = point(genealogy)
@@ -20,6 +28,12 @@ function occ(genealogy::Dict{Int64, Individual}, probandIDs::Vector{Int64}, ance
     end
 end
 
+"""
+occur!(individual::PointerIndividual)::
+
+A recursive function that increments the occurrence of a pointer `individual`
+if they are an ancestor.
+"""
 function occur!(individual::PointerIndividual)::Nothing
     if individual.state == ANCESTOR
         individual.occurrence += 1
