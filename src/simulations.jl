8dea0165fbb2be378694b3bf56a2857fabafc2d9
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-20 11:04:38 -0500

    Add comments

--- src/simulations.jl
+++ src/simulations.jl
@@ -1,19 +1,34 @@
-function simulate_allele(genealogy::Dict{Int64, Individual}, ancestorID::Int64, probandIDs::Vector{Int64}, frequency::Float64, max_simulations::Int64)::Vector{Int64}
+"""
+simulate_allele(genealogy::Dict{Int64, Individual}, ancestorID::Int64, probandIDs::Vector{Int64}, frequency::Float64, patience::Int64)::Vector{Int64}
+
+Takes a given `genealogy`, an `ancestorID`, a vector of `probandIDs`,
+an allele `frequency` and a maximum number of simulations (`patience`)
+and simulates an allele's dropping from that ancestor.
+Returns the IDs of probands who carry the minor allele (`:m`).
+If patience is exhausted, the affected probands from the last simulation are given.
+"""
+function simulate_allele(genealogy::Dict{Int64, Individual}, ancestorID::Int64, probandIDs::Vector{Int64}, frequency::Float64, patience::Int64)::Vector{Int64}
     number_of_simulations = 1
     current_frequency = 0
-    affected = Vector{Int64}()
+    carriers = Vector{Int64}()
     pointer = point(genealogy)
     ancestor = pointer[ancestorID]
-    ancestor.genotype = [:m, :M]
-    while (current_frequency < 0.9 * frequency) | (current_frequency > 1.1 * frequency) & (number_of_simulations ≤ max_simulations)
+    ancestor.genotype = [:M, :m] # :M is for the major allele, :m is for the minor allele
+    while (current_frequency < 0.9 * frequency) | (current_frequency > 1.1 * frequency) & (number_of_simulations ≤ patience)
         transmit!(ancestor)
-        affected = [probandID for probandID in probandIDs if :m ∈ pointer[probandID].genotype]
+        carriers = [probandID for probandID in probandIDs if :m ∈ pointer[probandID].genotype]
         current_frequency = length(affected) / length(probandIDs)
         number_of_simulations += 1
     end
-    affected
+    carriers
 end
 
+"""
+transmit!(individual::PointerIndividual)::Nothing
+
+A recursive function that randomly transmits one of an
+`individual`'s two alleles down do their children.
+"""
 function transmit!(individual::PointerIndividual)::Nothing
     for child in individual.children
         transmitted_allele = rand(individual.genotype)
