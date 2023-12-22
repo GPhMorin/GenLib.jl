8fc7208e3579917cf08416d71745bde523fab4d2
Gilles-Philippe Morin <gphmorin@gmail.com>
2023-12-21 21:10:13 -0500

    Use vector instead of set

--- src/mrca.jl
+++ src/mrca.jl
@@ -28,7 +28,7 @@ ancestor(genealogy::Dict{Int64, Individual}, IDs::Set{Int64})::Set{Int64}
 
 Takes a `genealogy` dictionary and a set of `IDs` and returns a set of ancestors.
 """
-function ancestor(genealogy::Dict{Int64, Individual}, IDs::Set{Int64})::Vector{Int64}
+function ancestor(genealogy::Dict{Int64, Individual}, IDs::Vector{Int64})::Vector{Int64}
     ancestors = ∪(ancestor(genealogy, ID) for ID in IDs)
     sort!(ancestors)
 end
