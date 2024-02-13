3cd7357e5e770654d27f0eb4b1e8f9ac656b522b
Gilles-Philippe Morin <gphmorin@gmail.com>
2024-02-13 15:13:16 -0500

    add test for children

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,7 @@ using Test
     ped = gen.genealogy(genea140)
     @test repr(MIME("text/plain"), ped) == "A pedigree with:\n41523 individuals;\n68248 parent-child relations;\n20773 men;\n20750 women;\n140 subjects;\n18 generations."
     @test repr(MIME("text/plain"), ped[33724]) == "ind: 33724\nfather: 10086\nmother: 10087\nsex: 1"
+    @test gen.children(ped, 33724) == [10033, 113470]
     @test ped[33724].children[2].father.ID == 33724
     @test sum(gen.gc(ped)) == 140
 
