c286c8eaa4fc9237b6b4317a78cf209e3deeb3b8
Gilles-Philippe Morin <gphmorin@gmail.com>
2024-09-28 01:41:07 -0400

    Revert

--- src/compute.jl
+++ src/compute.jl
@@ -428,21 +428,25 @@ function probands_sparse_phi(pedigree::Pedigree, probandIDs::Vector{Int64} = pro
             for ID ∈ previous_generationIDs
                 indexed_pedigree[ID].founder_index = 1
             end
-            # Make a vector for the new kinships
-            ϕs = Vector{Pair{Tuple{Int32, Int32}, Float64}}()
-            # Fill the vector in order, using the adapted algorithm from Karigl, 1981
-            individuals = [indexed_pedigree[ID] for ID ∈ sort(next_generationIDs)]
-            for i ∈ eachindex(individuals), j ∈ eachindex(individuals)
-                individualᵢ = individuals[i]
-                individualⱼ = individuals[j]
-                if individualᵢ.ID ≤ individualⱼ.ID
-                    coefficient = phi(individualᵢ, individualⱼ, ϕ)
-                    if coefficient > 0
-                        push!(ϕs, (individualᵢ.ID, individualⱼ.ID) => coefficient)
+            # Make a parallel copy of the kinships
+            ϕs = [Vector{Pair{Tuple{Int32, Int32}, Float64}}() for _ ∈ 1:Threads.nthreads()]
+            # Fill the vector in parallel, using the adapted algorithm from Karigl, 1981
+            individuals = [indexed_pedigree[ID] for ID ∈ next_generationIDs]
+            Threads.@threads for i ∈ eachindex(individuals)
+                Threads.@threads for j ∈ eachindex(individuals)
+                    individualᵢ = individuals[i]
+                    individualⱼ = individuals[j]
+                    if individualᵢ.ID ≤ individualⱼ.ID
+                        coefficient = phi(individualᵢ, individualⱼ, ϕ)
+                        if coefficient > 0
+                            push!(ϕs[Threads.threadid()], (individualᵢ.ID, individualⱼ.ID) => coefficient)
+                        end
                     end
                 end
             end
-            ϕ = ϕs
+            ϕ = pop!(ϕs)
+            append!(ϕ, ϕs...)
+            sort!(ϕ)
         end
         if verbose
             println("Transforming the kinships into a sparse CSC matrix.")
