5bd6a24ef0b01c914ee64735cb2b3d6218331a1b
Gilles-Philippe Morin <gphmorin@gmail.com>
2024-09-28 11:29:17 -0400

    Fix merge

--- src/compute.jl
+++ src/compute.jl
@@ -184,7 +184,6 @@ function _previous_generation(pedigree::Pedigree, next_generationIDs::Vector{Int
     end
     unique!(previous_generationIDs)
     sort!(previous_generationIDs)
-    sort!(previous_generationIDs)
 end
 
 """
@@ -430,9 +429,8 @@ function probands_sparse_phi(pedigree::Pedigree, probandIDs::Vector{Int64} = pro
             for ID ∈ previous_generationIDs
                 indexed_pedigree[ID].founder_index = 1
             end
-            # List all pairs to split them into chunks
-            pairs = [(IDᵢ, IDⱼ) for IDᵢ ∈ next_generationIDs for IDⱼ ∈ next_generationIDs]
-            chunks = Iterators.partition(pairs, length(pairs) ÷ Threads.nthreads())
+            # Make a parallel copy of the kinships
+            ϕs = [Vector{Pair{Tuple{Int32, Int32}, Float64}}() for _ ∈ 1:Threads.nthreads()]
             # Fill the vector in parallel, using the adapted algorithm from Karigl, 1981
             individuals = [indexed_pedigree[ID] for ID ∈ next_generationIDs]
             Threads.@threads :static for i ∈ eachindex(individuals)
@@ -445,7 +443,6 @@ function probands_sparse_phi(pedigree::Pedigree, probandIDs::Vector{Int64} = pro
                             push!(ϕs[Threads.threadid()], (individualᵢ.ID, individualⱼ.ID) => coefficient)
                         end
                     end
-                    ϕs
                 end
             end
             # Merge the vectors and sort them
